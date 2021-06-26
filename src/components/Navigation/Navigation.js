f94ec2dc148dfad994bfad8b003f0082d6055a60
Igor Kreshchenko <Igor.kreshchenko@gmail.com>
2021-06-26 12:41:39 +0300

    Добавляет PrivateRoute и PublicRoute

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import styles from './Navigation.module.css';
+import { authSelectors } from '../../redux/auth';
 
-const Navigation = () => (
+const Navigation = ({ isAuthenticated }) => (
   <nav>
     <NavLink
       to="/"
@@ -13,15 +15,21 @@ const Navigation = () => (
       Главная
     </NavLink>
 
-    <NavLink
-      to="/contacts"
-      exact
-      className={styles.link}
-      activeClassName={styles.activeLink}
-    >
-      Контакты
-    </NavLink>
+    {isAuthenticated && (
+      <NavLink
+        to="/contacts"
+        exact
+        className={styles.link}
+        activeClassName={styles.activeLink}
+      >
+        Контакты
+      </NavLink>
+    )}
   </nav>
 );
 
-export default Navigation;
+const mapStateToProps = state => ({
+  isAuthenticated: authSelectors.getIsAuthenticated(state),
+});
+
+export default connect(mapStateToProps)(Navigation);
